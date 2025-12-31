3e4da1d873372197893b18b6f7251887197e31ba
Peculiars <olaitandamilare230@gmail.com>
2025-12-31 11:12:25 +0100

    feat: save

--- src/features/landing/Hero.tsx
+++ src/features/landing/Hero.tsx
@@ -1,27 +1,16 @@
 "use client"
-import { useEffect, useState } from 'react';
 import MobileHeroCarousel from './MobileHeroCarousel';
 import HeroCarousel from './DesktopHeroCarousel';
 
 export default function Hero() {
-  const [isMobile, setIsMobile] = useState<boolean | null>(null);
-
-  useEffect(() => {
-    const checkMobile = (): void => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
-  }, []);
-
-
-  if (isMobile === null) {
-    return (
-      <div className="relative w-full h-[90vh] md:h-[650px] bg-[#6B46C1]" />
-    );
-  }
-
-  return isMobile ? <MobileHeroCarousel /> : <HeroCarousel />;
+  return (
+    <>
+      <div className="block md:hidden">
+        <MobileHeroCarousel />
+      </div>
+      <div className="hidden md:block">
+        <HeroCarousel />
+      </div>
+    </>
+  );
 }
