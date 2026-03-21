a9ca74997bd829f13d8a67543999e775e308ce82
Olaitan Damilare <olaitandamilare230@gmail.com>
2026-03-21 20:33:29 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -21,6 +19,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  
+  // ✅ Add this as well if the above doesn't work
+  serverRuntimeConfig: {
+    maxDuration: 60, // Maximum execution time in seconds
+  },
 };
-
-export default nextConfig; 
